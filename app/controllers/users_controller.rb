90f2bed8ae84bf9abef8128fbf17ecc7b7bf9103
Mridul Kumar <mridul.k@browserstack.com>
2022-10-18 13:39:52 +0530

    minor fix for user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,17 @@ class UsersController < ApplicationController
 
     def new
         if session[:user_id]
-            redirect_to '/' if User.find(session[:user_id])
+            redirect_to '/login' if User.find(session[:user_id])
         else
             @user = User.new
         end
     end
 
     def create
-        @user = User.new
+        @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to '/'
+            redirect_to '/login'
         else
             render :new
         end
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
             render :edit
         end
     end
-
+ 
     private
     def user_params
         params.require(:user).permit(:user_name, :full_name, :email, :password)
