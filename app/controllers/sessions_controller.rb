805595147b96313718ccc801034eeed61de1482d
Mridul Kumar <mridul.k@browserstack.com>
2022-10-28 17:52:20 +0530

    minor fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
             @user = User.find_by(user_name: user_name) if user_name
             if @user && @user.authenticate(@params[:password])
                 session[:user_id] = @user.id
-                redirect_to '/'
+                redirect_to root_path
             else
                 render :new
             end
